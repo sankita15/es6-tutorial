7a8033acc36bf7a88bbb41a8f5bb34e2885fd079
Ankita Srivastava <Ankita_Srivastava@gap.com>
2019-09-24 17:43:08 +0530

    create sonar issues

---
+++ HelloWorld.java
@@ -0,0 +1,19 @@
+public class HelloWorld{
+    public static void main(String[] args) {
+        if(1 == 1){
+            System.out.println("kedar");
+        }
+        if(1 == 1){
+            System.out.println("santhosh");
+        }
+        if(1 == 1){
+            System.out.println("arbaz");
+        }
+        if(1 == 1){
+            System.out.println("ankita");
+        }
+        if(1 == 1){
+            System.out.println("UI Migration");
+        }
+    }
+}
